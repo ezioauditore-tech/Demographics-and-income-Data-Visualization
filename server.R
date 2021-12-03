0913e459309c2f378b8396d4e1b292a4b6c800a0
Sakthivel SenthilKumar <59603871+ezioauditore-tech@users.noreply.github.com>
2021-12-03 12:33:22 +0530

    Update server.R

--- server.R
+++ server.R
@@ -1,34 +1,34 @@
-# Load libraries
+
 library(shiny)
 library(tidyverse)
 library(ggplot2)
 library(ggthemes)
-# Read in data
+
 adult <- read_csv("adult.csv")
-# Convert column names to lowercase for convenience 
+
 names(adult) <- tolower(names(adult))
 
-# Define server logic
+
 shinyServer(function(input, output) {
   
   df_country <- reactive({
     adult %>% filter(native_country == input$country)
   })
   
-  # TASK 5: Create logic to plot histogram or boxplot
+  
   output$p1 <- renderPlot({
     if (input$graph_type == "Histogram") {
       # Histogram
       ggplot(df_country(), aes_string(x =input$continuous_variable)) +
         geom_histogram() +  # histogram geom
-        title(ylab = "people",main="Histogram")+  # labels
-        facet_wrap(~prediction)# facet by prediction
+        title(ylab = "people",main="Histogram")+  
+        facet_wrap(~prediction)
     }
     else {
       # Boxplot
       ggplot(df_country(), aes_string(y = input$continuous_variable )) +
-        geom_boxplot() +  # boxplot geom
-        coord_flip() +  # flip coordinates
+        geom_boxplot() +  
+        coord_flip() + 
         title(ylab = "people",main = "Boxplot")+ theme(
           panel.background = element_rect(fill = "lightblue",
                                           colour = "lightblue",
@@ -37,26 +37,25 @@ shinyServer(function(input, output) {
                                           colour = "white"), 
           panel.grid.minor = element_line(size = 0.25, linetype = 'solid',
                                           colour = "white")
-        )+ # labels
-        facet_wrap(~prediction) # facet by prediction
+        )+ 
+        facet_wrap(~prediction) 
     }
     
   })
   
-  # TASK 6: Create logic to plot faceted bar chart or stacked bar chart
   output$p2 <- renderPlot({
     # Bar chart
     p <- ggplot(df_country(), aes_string(x = input$categorical_variable )) +
-      title(ylab = "people",main="Barchart") +  # labels
-      theme(axis.text.x =element_text(angle = 45))+theme(legend.position="bottom")    # modify theme to change text angle and legend position
+      title(ylab = "people",main="Barchart") + 
+      theme(axis.text.x =element_text(angle = 45))+theme(legend.position="bottom")    
     
     if (input$is_stacked) {
-      p +geom_bar(aes_string(fill= "prediction"))  # add bar geom and use prediction as fill
+      p +geom_bar(aes_string(fill= "prediction")) 
     }
     else{
       p + 
-        geom_bar(aes_string(fill=input$categorical_variable)) + # add bar geom and use input$categorical_variables as fill 
-        facet_wrap(~prediction)   # facet by prediction
+        geom_bar(aes_string(fill=input$categorical_variable)) +  
+        facet_wrap(~prediction)  
     }
   })
   
